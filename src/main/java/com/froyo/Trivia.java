ed00f71f8fad6d05fb16f43097a040a2b8121d57
Nicolas-Chambon <n.chambon84@gmail.com>
2021-01-20 12:06:21 +0100

    optimize

--- src/main/java/com/froyo/Trivia.java
+++ src/main/java/com/froyo/Trivia.java
@@ -5,15 +5,15 @@ import java.util.LinkedList;
 
 public class Trivia {
 
-    ArrayList players = new ArrayList();
+    ArrayList<String> players = new ArrayList<>();
     int[] places = new int[6];
     int[] purses  = new int[6];
     boolean[] inPenaltyBox  = new boolean[6];
     
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
+    LinkedList<String> popQuestions = new LinkedList<>();
+    LinkedList<String> scienceQuestions = new LinkedList<>();
+    LinkedList<String> sportsQuestions = new LinkedList<>();
+    LinkedList<String> rockQuestions = new LinkedList<>();
 
     static final String SPORTS = "Sports";
 	static final String POP = "Pop";
